52195e5408d1086a59fe2ccb02c57bf2c79eaaaf
MatthiasWanner <matthias.wanner@gmail.com>
2022-02-25 19:11:59 +0100

    :sparkles: (app) Apply form component + set lint staged

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" width="100" height="100" />
-  <HelloWorld msg="Hello World" />
-</template>
-
 <script setup lang="ts">
   // This starter template is using Vue 3 <script setup> SFCs
   // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-  import HelloWorld from './components/HelloWorld.vue'
+  import Form from './components/Form/index.vue'
 </script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" width="100" height="100" />
+  <Form />
+</template>
 
 <style>
   #app {
