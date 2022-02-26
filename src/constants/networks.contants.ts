32ef5726d8a22f758d07bdd19b0df8d0b752c669
MatthiasWanner <matthias.wanner@gmail.com>
2022-02-26 17:37:43 +0100

    :construction: (constants) Networks that will be used in select options

---
+++ src/constants/networks.contants.ts
@@ -0,0 +1,44 @@
+const networks = [
+  {
+    name: 'Mainnet',
+    address: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    chainId: '0x1',
+  },
+  {
+    name: 'Ropsten',
+    address: 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    chainId: '0x3',
+  },
+  {
+    name: 'Rinkeby',
+    address: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    chainId: '0x4',
+  },
+  {
+    name: 'Kovan',
+    address: 'https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    chainId: '0x6',
+  },
+  {
+    name: 'Matic',
+    address: 'https://polygon-rpc.com/',
+    chainId: '0x89',
+  },
+  {
+    name: 'Fantom',
+    address: 'https://rpc.ftm.tools',
+    chainId: '0xfa',
+  },
+  {
+    name: 'Binance',
+    address: 'https://bsc-dataseed.binance.org',
+    chainId: '0x38',
+  },
+  {
+    name: 'XDai',
+    address: 'https://rpc.xdaichain.com',
+    chainId: '0x64',
+  },
+];
+
+export default networks;
