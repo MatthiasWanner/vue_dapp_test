62d972058a141b826e92eff77fd7e870363bb2b4
MatthiasWanner <matthias.wanner@gmail.com>
2022-02-27 11:26:14 +0100

    :building_construction: (constants) Add explorer urls to networks constants

--- src/constants/networks.contants.ts
+++ src/constants/networks.contants.ts
@@ -1,43 +1,53 @@
-const networks = [
+import { INetwork, Networks } from '../types/useWindowEthereum.types';
+
+const networks: INetwork[] = [
   {
-    name: 'Mainnet',
+    name: Networks.MAINNET,
     address: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
     chainId: '0x1',
+    blockExplorerUrl: 'https://etherscan.io',
   },
   {
-    name: 'Ropsten',
+    name: Networks.ROPSTEN,
     address: 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
     chainId: '0x3',
+    blockExplorerUrl: 'https://ropsten.etherscan.io',
   },
   {
-    name: 'Rinkeby',
+    name: Networks.RINKEBY,
     address: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
     chainId: '0x4',
+    blockExplorerUrl: 'https://rinkey.etherscan.io',
   },
   {
-    name: 'Kovan',
+    name: Networks.KOVAN,
     address: 'https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
     chainId: '0x6',
+    blockExplorerUrl: 'https://kovan.etherscan.io',
   },
   {
-    name: 'Matic',
+    name: Networks.MATIC,
     address: 'https://polygon-rpc.com/',
     chainId: '0x89',
+    blockExplorerUrl: 'https://polygonscan.com/',
   },
   {
-    name: 'Fantom',
+    name: Networks.FANTOM,
     address: 'https://rpc.ftm.tools',
     chainId: '0xfa',
+    blockExplorerUrl: 'https://ftmscan.com',
   },
   {
-    name: 'Binance',
+    name: Networks.BINANCE,
     address: 'https://bsc-dataseed.binance.org',
     chainId: '0x38',
+    blockExplorerUrl: 'https://bscscan.com',
   },
   {
-    name: 'XDai',
+    name: Networks.XDAI,
     address: 'https://rpc.xdaichain.com',
     chainId: '0x64',
+    blockExplorerUrl: 'https://blockscout.com/xdai/mainnet',
   },
 ];
 
