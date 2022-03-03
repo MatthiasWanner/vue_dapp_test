d84729cd2438d795d172831220034c9ee8854677
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-03 21:12:44 +0100

    :lipstick: (app) Apply app baground color and padding

--- src/App.vue
+++ src/App.vue
@@ -18,15 +18,24 @@
 </template>
 
 <style>
+  body {
+    margin: 0;
+  }
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 20px;
+    padding: 10px;
     position: relative;
     max-width: 1400px;
+    background-image: linear-gradient(
+      0deg,
+      #fdfdff 0,
+      #7f7f80 10%,
+      #000000 100%
+    );
   }
 
   .vue-logo {
